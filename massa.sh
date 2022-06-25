9852bdc03352579a01c5faf42aafd1b540692894
mdlog <adiadi2411@gmail.com>
2022-06-25 14:37:07 +0800

    Update massa.sh

--- massa.sh
+++ massa.sh
@@ -16,11 +16,11 @@ echo -e "\e[1m\e[32m1. Delete Folder Massa dan massa.sh... \e[0m" && sleep 1
 rm -rf massa
 rm -rf massa.sh
 
-echo -e "\e[1m\e[32m1. Updating packages... \e[0m" && sleep 1
+echo -e "\e[1m\e[32m2. Updating packages... \e[0m" && sleep 1
 # update
 #sudo apt update 
 
-echo -e "\e[1m\e[32m2. Installing dependencies... \e[0m" && sleep 1
+echo -e "\e[1m\e[32m3. Installing dependencies... \e[0m" && sleep 1
 # packages
 #sudo apt install pkg-config curl git build-essential libssl-dev libclang-dev
 #sudo apt-get install librocksdb-dev build-essential
@@ -33,7 +33,7 @@ source $HOME/.cargo/env
 rustup toolchain install nightly
 rustup default nightly
 
-echo -e "\e[1m\e[32m3. Downloading and building... \e[0m" && sleep 1
+echo -e "\e[1m\e[32m4. Downloading and building... \e[0m" && sleep 1
 # download binary
 cd $HOME
 git clone --branch testnet https://github.com/massalabs/massa.git
