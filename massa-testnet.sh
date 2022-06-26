7cda0c57001caf0f77746da5456d0b1d74355d07
mdlog <adiadi2411@gmail.com>
2022-06-26 15:45:19 +0800

    Update massa-testnet.sh

--- massa-testnet.sh
+++ massa-testnet.sh
@@ -50,7 +50,7 @@ cd $HOME
 cd massa/massa-node/config
 wget https://raw.githubusercontent.com/mdlog/massa-mdlog/main/massa/config.toml
 sed -i -e "s/^routable_ip *=.*/AAA.BBB.CCC.DDD = \"$IP_SERVER\"/" $HOME/massa/massa-node/config/config.toml
-sed -i -e "s/^AAA.BBB.CCC.DDD *=.*/routable_ip $HOME/massa/massa-node/config/config.toml
+sed -i -e "s/^AAA.BBB.CCC.DDD *=.*/routable_ip = \"routable_ip\"/" $HOME/massa/massa-node/config/config.toml
 
 sudo tee /root/massa/massa-node/run.sh > /dev/null <<EOF
 #!/bin/bash
