3b9ea94017350aec9e8fe53a28ee5a1cc2edaff9
mdlog <adiadi2411@gmail.com>
2022-06-26 14:19:50 +0800

    Update massa-testnet.sh

--- massa-testnet.sh
+++ massa-testnet.sh
@@ -25,22 +25,21 @@ echo "export SEI_PORT=${SEI_PORT}" >> $HOME/.bash_profile
 source $HOME/.bash_profile
 
 
-echo -e "\e[1m\e[32m1. Delete Folder Massa dan massa.sh... \e[0m" && sleep 1
 #delete folder
 rm -rf massa
 rm -rf massa-test.sh
 rm -rf massa-testnet.sh
 rm -rf massa_TEST.11.3_release_linux.tar.gz
 
-echo -e "\e[1m\e[32m2. Updating packages... \e[0m" && sleep 1
+echo -e "\e[1m\e[32m1. Updating packages... \e[0m" && sleep 1
 # update
-sudo apt-get update && apt-get upgrade
+sudo apt-get update
 sudo apt install screen
-echo -e "\e[1m\e[32m3. Installing dependencies... \e[0m" && sleep 1
+echo -e "\e[1m\e[32m2. Installing dependencies... \e[0m" && sleep 1
 # packages
 sudo apt install pkg-config curl git build-essential libssl-dev libclang-dev
 sudo apt-get install librocksdb-dev build-essential
-echo -e "\e[1m\e[32m4. Downloading and building massa binary... \e[0m" && sleep 1
+echo -e "\e[1m\e[32m3. Downloading and building massa binary... \e[0m" && sleep 1
 # download binary
 cd $HOME
 wget https://github.com/massalabs/massa/releases/download/TEST.11.3/massa_TEST.11.3_release_linux.tar.gz
