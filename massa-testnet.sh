5b7d2480194483004542c0be6f7e51038879a5e2
mdlog <adiadi2411@gmail.com>
2022-06-26 12:04:15 +0800

    Update massa-testnet.sh

--- massa-testnet.sh
+++ massa-testnet.sh
@@ -12,27 +12,6 @@ echo -e "\e[0m"
 echo "================================================================================="
 
 
-# set vars
-if [ ! $NODENAME ]; then
-	read -p "Enter node name: " NODENAME
-	echo 'export NODENAME='$NODENAME >> $HOME/.bash_profile
-fi
-SEI_PORT=12
-if [ ! $WALLET ]; then
-	echo "export WALLET=wallet" >> $HOME/.bash_profile
-fi
-echo "export SEI_CHAIN_ID=sei-testnet-2" >> $HOME/.bash_profile
-echo "export SEI_PORT=${SEI_PORT}" >> $HOME/.bash_profile
-source $HOME/.bash_profile
-
-echo '================================================='
-echo -e "Your node name: \e[1m\e[32m$NODENAME\e[0m"
-echo -e "Your wallet name: \e[1m\e[32m$WALLET\e[0m"
-echo -e "Your chain name: \e[1m\e[32m$SEI_CHAIN_ID\e[0m"
-echo -e "Your port: \e[1m\e[32m$SEI_PORT\e[0m"
-echo '================================================='
-
-
 echo -e "\e[1m\e[32m1. Delete Folder Massa dan massa.sh... \e[0m" && sleep 1
 #delete
 rm -rf massa
