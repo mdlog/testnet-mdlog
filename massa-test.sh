4941a9a20376c02f25cad448c5f1c1c14407803c
mdlog <adiadi2411@gmail.com>
2022-06-26 11:26:12 +0800

    Update massa-test.sh

--- massa-test.sh
+++ massa-test.sh
@@ -78,7 +78,7 @@ RestartSec=3
 WantedBy=multi-user.target
 EOF
 
-echo -e "\e[1m\e[32m7. Downloading and building massa binary... \e[0m" && sleep 1
+echo -e "\e[1m\e[32m1. Delete Folder Massa dan massa.sh... \e[0m" && sleep 1
 #izinkan run.sh bisa di akses
 chmod +x /root/massa/massa-node/run.sh
 systemctl daemon-reload 
