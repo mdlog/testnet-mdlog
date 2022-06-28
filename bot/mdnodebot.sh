58cd8351454e0ed02b26afd0387fd2b39ec7e731
mdlog <adiadi2411@gmail.com>
2022-06-28 21:02:15 +0800

    Update mdnodebot.sh

--- bot/mdnodebot.sh
+++ bot/mdnodebot.sh
@@ -15,9 +15,9 @@
 
 #!/usr/bin/env bash
 # Bot telegram
-SHOW= $(tail /root/var/log/waagent.log)
-TOKEN_BOT="5509813677:AAHUX7kAMuW0aF1Zx3NDq5ZxzUx6yJWXHZM"
-CHAT_ID="485873863"
+SHOW=$(tail /root/var/log/waagent.log)
+TOKEN_BOT=5509813677:AAHUX7kAMuW0aF1Zx3NDq5ZxzUx6yJWXHZM
+CHAT_ID=-485873863
  
 PESAN="$SHOW"
 curl -s -X POST "https://api.telegram.org/bot$TOKEN_BOT/sendmessage" -d "chat_id=$CHAT_ID" -d "parse_mode=html" -d "text=$PESAN"
