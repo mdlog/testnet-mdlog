264f5860b2bcc3ef1f6b3e472a0601eccdbf84d5
mdlog <adiadi2411@gmail.com>
2022-06-28 20:26:42 +0800

    Update mdnodebot.sh

--- bot/mdnodebot.sh
+++ bot/mdnodebot.sh
@@ -1,13 +1,22 @@
-#!/bin/bash
+##!/bin/bash
 
-clear
-SHOW=$(tail -f /root/massa/massa-node/logs.txt)
-TOKEN=5509813677:AAHUX7kAMuW0aF1Zx3NDq5ZxzUx6yJWXHZM
-CHAT_ID=485873863
-MESSAGE=”$SHOW”
+#clear
+#SHOW=$(tail -f /root/massa/massa-node/logs.txt)
+#TOKEN=5509813677:AAHUX7kAMuW0aF1Zx3NDq5ZxzUx6yJWXHZM
+#CHAT_ID=485873863
+#MESSAGE=”$SHOW”
 
-URL=https://api.telegram.org/bot$TOKEN/sendMessage
-curl -s -X POST $URL –d chat_id -d text=”$MESSAGE”
+#URL=https://api.telegram.org/bot$TOKEN/sendMessage
+#curl -s -X POST $URL –d chat_id -d text=”$MESSAGE”
 
 
-eof
+#eof
+
+
+#!/usr/bin/env bash
+# Bot telegram
+TOKEN_BOT="5509813677:AAHUX7kAMuW0aF1Zx3NDq5ZxzUx6yJWXHZM"
+CHAT_ID="485873863"
+ 
+PESAN="Halo dikirim dari server <b>$(hostname)</b>"
+curl -s -X POST "https://api.telegram.org/bot$TOKEN_BOT/sendmessage" -d "chat_id=$CHAT_ID" -d "parse_mode=html" -d "text=$PESAN"
