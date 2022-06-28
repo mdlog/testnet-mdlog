ca20777dfdf1da90b8192748c57e991cb88ca672
mdlog <adiadi2411@gmail.com>
2022-06-28 20:33:59 +0800

    Update mdnodebot.sh

--- bot/mdnodebot.sh
+++ bot/mdnodebot.sh
@@ -18,5 +18,5 @@
 TOKEN_BOT="5509813677:AAHUX7kAMuW0aF1Zx3NDq5ZxzUx6yJWXHZM"
 CHAT_ID="485873863"
  
-PESAN="Halo dikirim dari server <b>$(hostname)</b>"
+PESAN="Halo dikirim dari server <b>$(tail -f /root/massa/massa-node/logs.txt)</b>"
 curl -s -X POST "https://api.telegram.org/bot$TOKEN_BOT/sendmessage" -d "chat_id=$CHAT_ID" -d "parse_mode=html" -d "text=$PESAN"
