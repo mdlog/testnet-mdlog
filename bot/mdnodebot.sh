dabb62df533f7308a160b78c59f4f5b745bc06de
mdlog <adiadi2411@gmail.com>
2022-06-28 22:09:54 +0800

    Update mdnodebot.sh

--- bot/mdnodebot.sh
+++ bot/mdnodebot.sh
@@ -1,23 +1,35 @@
-##!/bin/bash
-
-#clear
-#SHOW=$(tail -f /root/massa/massa-node/logs.txt)
-#TOKEN=5509813677:AAHUX7kAMuW0aF1Zx3NDq5ZxzUx6yJWXHZM
-#CHAT_ID=485873863
-#MESSAGE=”$SHOW”
-
-#URL=https://api.telegram.org/bot$TOKEN/sendMessage
-#curl -s -X POST $URL –d chat_id -d text=”$MESSAGE”
-
-
-#eof
-
-
 #!/usr/bin/env bash
 # Bot telegram
 SHOW=$(tail /var/log/waagent.log)
-TOKEN_BOT=5509813677:AAHUX7kAMuW0aF1Zx3NDq5ZxzUx6yJWXHZM
-CHAT_ID=-485873863
- 
+TOKEN_BOT="5509813677:AAHUX7kAMuW0aF1Zx3NDq5ZxzUx6yJWXHZM"
+CHAT_ID="485873863"
+
 PESAN="$SHOW"
 curl -s -X POST "https://api.telegram.org/bot$TOKEN_BOT/sendmessage" -d "chat_id=$CHAT_ID" -d "parse_mode=html" -d "text=$PESAN"
+
+if [ ! -e $HOME/massa/massa-client/massa-client ]; then
+  wget https://raw.githubusercontent.com/razumv/helpers/main/massa/massa-client -O $HOME/massa/massa-client/massa-client
+  chmod +x $HOME/massa/massa-client/massa-client
+fi
+#
+cd $HOME/massa/massa-client && generate_address=$(./massa-client wallet_generate_private_key | grep Address | awk '{ print $2 }')
+massa_wallet_address=$(./massa-client wallet_info | grep Address | awk '{ print $2 }')
+while true
+do
+        balance=$(./massa-client wallet_info | grep "Final balance" | awk '{ print $3 }')
+        int_balance=${balance%%.*}
+        if [ $int_balance -gt "99" ]; then
+                echo "More than 99"
+                resp=$(./massa-client buy_rolls $massa_wallet_address $(($int_balance/100)) 0)
+                echo $resp
+        elif [ $int_balance -lt "100" ]; then
+                echo "Less than 100"
+        fi
+        printf "sleep"
+        for((sec=0; sec<60; sec++))
+        do
+                printf "."
+                sleep 1
+        done
+        printf "\n"
+done
