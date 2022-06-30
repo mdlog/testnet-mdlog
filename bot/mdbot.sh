fd1979bdd295570b7cacb6c667cfb821068c59dc
mdlog <adiadi2411@gmail.com>
2022-06-30 20:15:45 +0800

    Update mdbot.sh

--- bot/mdbot.sh
+++ bot/mdbot.sh
@@ -9,8 +9,20 @@ cd $HOME/massa/massa-client
 active_rolls=$(./massa-client wallet_info | grep "Active rolls" | awk '{ print $3 }')
 int_rolls=${active_rolls}
 
+# update
+final_balance=$(./massa-client wallet_info | grep "Final balance" | awk '{ print $3 }')
+int_rolls=${final_balance}
+candidate_balance=$(./massa-client wallet_info | grep "Candidate balance" | awk '{ print $3 }')
+int_candidate_balance=${candidate_balance}
+locked_balance=$(./massa-client wallet_info | grep "Locked balance" | awk '{ print $3 }')
+int_locked_balance=${locked_balance}
+final_rolls=$(./massa-client wallet_info | grep "Final rolls" | awk '{ print $3 }')
+int_final_rolls=${final_rolls}
+candidate_rolls=$(./massa-client wallet_info | grep "Candidate rolls" | awk '{ print $3 }')
+int_rolls=${candidate_rolls}
+
 if  [ $int_rolls -gt "0" ]; then
-        curl -s -X POST "https://api.telegram.org/bot$TOKEN_BOT/sendmessage" -d "chat_id=$ID" -d "parse_mode=html" -d "text= ✅ NODE: $ADDRESS Aktif"
+        curl -s -X POST "https://api.telegram.org/bot$TOKEN_BOT/sendmessage" -d "chat_id=$ID" -d "parse_mode=html" -d "text= ✅ NODE: $ADDRESS Aktif\n Final balance : $"
 
 elif [ $int_rolls  -lt "1" ]; then
 
