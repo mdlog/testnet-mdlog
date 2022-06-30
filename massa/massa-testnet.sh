3ed8f6abc8041b26ef1ea7ea6a7cd7ebd246d71d
mdlog <adiadi2411@gmail.com>
2022-06-30 19:10:38 +0800

    Update massa-testnet.sh

--- massa/massa-testnet.sh
+++ massa/massa-testnet.sh
@@ -34,7 +34,9 @@ rm -rf massa_TEST.11.3_release_linux.tar.gz
 echo -e "\e[1m\e[32m1. Updating packages... \e[0m" && sleep 1
 # update
 sudo apt-get update
-sudo apt install screen
+sudo apt-get install clang
+sudo apt-get -y install librocksdb-dev
+
 echo -e "\e[1m\e[32m2. Installing dependencies... \e[0m" && sleep 1
 
 # packages
