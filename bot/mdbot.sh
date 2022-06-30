314a11c7c7777d333d8f7d4da204fb87b2eda379
mdlog <adiadi2411@gmail.com>
2022-06-30 22:20:13 +0800

    Update mdbot.sh

--- bot/mdbot.sh
+++ bot/mdbot.sh
@@ -6,7 +6,7 @@ TOKEN_BOT="5509813677:AAHUX7kAMuW0aF1Zx3NDq5ZxzUx6yJWXHZM"
 while true
 do
 cd $HOME/massa/massa-client
-active_rolls=$(./massa-client wallet_info | grep "Active rolls" | awk '{ print $1 }')
+active_rolls=$(./massa-client wallet_info | grep "Active rolls" | awk '{ print $3 }')
 int_rolls=${active_rolls}
 if  [ $int_rolls -gt "0" ]; then
         curl -s -X POST "https://api.telegram.org/bot$TOKEN_BOT/sendmessage" -d "chat_id=$ID" -d "parse_mode=html" -d "text= ✅ NODE: $ADDRESS Aktif"
