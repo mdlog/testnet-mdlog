65258547507f6cdde6684b4fa58a7ab37d90382c
mdlog <adiadi2411@gmail.com>
2022-07-10 00:45:33 +0800

    Update mdbotedit.sh

--- bot/mdbotedit.sh
+++ bot/mdbotedit.sh
@@ -3,23 +3,24 @@
 ADDRESS="$WALLET_ADDRESS"
 ID="$ID_CHAT"
 TOKEN_BOT="5509813677:AAHUX7kAMuW0aF1Zx3NDq5ZxzUx6yJWXHZM"
+PASSWORD="Makassar123"
 while true
 do
 cd $HOME/massa/massa-client
-active_rolls=$(./massa-client wallet_info | grep "Active rolls" | awk '{ print $3 }')
+active_rolls=$(./massa-client wallet_info -p $PASSWORD | grep "Active rolls" | awk '{ print $3 }')
 int_rolls=${active_rolls}
 
 # update
 
-final_balance=$(./massa-client wallet_info | grep "Final balance" | awk '{ print $3 }')
+final_balance=$(./massa-client wallet_info -p $PASSWORD | grep "Final balance" | awk '{ print $3 }')
 int_final_balance=${final_balance}
-candidate_balance=$(./massa-client wallet_info | grep "Candidate balance" | awk '{ print $3 }')
+candidate_balance=$(./massa-client wallet_info -p $PASSWORD | grep "Candidate balance" | awk '{ print $3 }')
 int_candidate_balance=${candidate_balance}
-locked_balance=$(./massa-client wallet_info | grep "Locked balance" | awk '{ print $3 }')
+locked_balance=$(./massa-client wallet_info -p $PASSWORD | grep "Locked balance" | awk '{ print $3 }')
 int_locked_balance=${locked_balance}
-final_rolls=$(./massa-client wallet_info | grep "Final rolls" | awk '{ print $3 }')
+final_rolls=$(./massa-client wallet_info -p $PASSWORD | grep "Final rolls" | awk '{ print $3 }')
 int_final_rolls=${final_rolls}
-candidate_rolls=$(./massa-client wallet_info | grep "Candidate rolls" | awk '{ print $3 }')
+candidate_rolls=$(./massa-client wallet_info -p $PASSWORD | grep "Candidate rolls" | awk '{ print $3 }')
 int_candidate_rolls=${candidate_rolls}
 AKTIF=" ✅ NODE: $ADDRESS Aktif "\==" Final balance: $int_final_balance "\==" Candidate balance: $int_candidate_balance "\==" Locked balance: $int_locked_balance "\==" Active rolls: $int_rolls "\==" Final rolls: $int_final_rolls"\==" Candidate rolls: $int_candidate_rolls"
 TIDAK=" ❌ NODE: $ADDRESS Tidak Aktif "\==" Final balance: $int_final_balance "\==" Candidate balance: $int_candidate_balance "\==" Locked balance: $int_locked_balance "\==" Active rolls: $int_rolls "\==" Final rolls: $int_final_rolls"\==" Candidate rolls: $int_candidate_rolls"
