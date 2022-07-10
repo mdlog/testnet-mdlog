3a7a51cada78517c0566774f0c3c140ec4d4a2a7
mdlog <adiadi2411@gmail.com>
2022-07-11 00:04:22 +0800

    Update massa-import.sh

--- massa/massa-import.sh
+++ massa/massa-import.sh
@@ -88,9 +88,8 @@ systemctl status massad
 
 
 #!/bin/bash
-if [ ! $PASSWORDKU ]; then
-read -p "Input Password Client Kamu: " PASSWORDKU
-echo 'export PASSWORDKU='\"${PASSWORDKU}\" >> $HOME/.bash_profile
+PASSWORD=$PASSWORDKU
+if [ ! $PRIVKEY ]; then
 read -p "Input Priv Key Kamu: " PRIVKEY
 echo 'export PRIVKEY='\"${PRIVKEY}\" >> $HOME/.bash_profile
 
@@ -98,15 +97,16 @@ fi
 echo 'source $HOME/.bashrc' >> $HOME/.bash_profile
 . $HOME/.bash_profile
 
-echo -e "Password Client Kamu: \e[1m\e[32m${PASSWORDKU}\e[0m"
+echo -e "Private Key Kamu: \e[1m\e[32m${PRIVKEY}\e[0m"
 echo '================================================='
+KEY=$PRIVKEY
 sleep 1
 # Generate New Wallet 
 
 clear
 cd ~
 chmod +x $HOME/massa/massa-client/massa-client
-cd $HOME/massa/massa-client && wallet_pss=$(./massa-client wallet_add_secret_keys $PRIVKEY -p $PASSWORDKU | grep Address )
+cd $HOME/massa/massa-client && wallet_priv_key=$(./massa-client wallet_add_secret_keys $KEY -p $PASSWORDKU | grep Address )
 # cd $HOME/massa/massa-client && generate_address=$(./massa-client wallet_add_secret_keys $PRIVKEY -p $PASSWORDKU | grep Address | awk '{ print $2 }')
 
 clear
