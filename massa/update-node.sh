2520bf14617d5d2fd2c89d944a172f68179bcc9b
mdlog <adiadi2411@gmail.com>
2022-07-13 22:57:29 +0800

    Update update-node.sh

--- massa/update-node.sh
+++ massa/update-node.sh
@@ -86,6 +86,3 @@ systemctl status massad
 clear
 cd $HOME
 wget -O buyrolls.sh https://raw.githubusercontent.com/mdlog/testnet-mdlog/main/massa/buyrolls.sh && chmod +x buyrolls.sh && screen -xR -S buyrolls ./buyrolls.sh
-
-sudo xdotool key ctrl+a+d
-
