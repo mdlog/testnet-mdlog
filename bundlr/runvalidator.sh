5909a16acaed5ebdbeb56a9944419c8648a17d26
mdlog <adiadi2411@gmail.com>
2022-07-16 02:21:15 +0800

    Update runvalidator.sh

--- bundlr/runvalidator.sh
+++ bundlr/runvalidator.sh
@@ -21,8 +21,7 @@ sudo apt-get install cargo
 sudo apt-get install screen
 sudo apt-get install pkg-config
 sudo apt-get install libssl-dev
-sudo apt-get install cron
-sudo apt-get install libsqlite3-dev
+sudo apt-get install -y libpq-dev
 sudo apt install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
