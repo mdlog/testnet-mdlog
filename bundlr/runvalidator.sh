762ab051cf5c728399cf84815c6c2f8b8974e2f1
mdlog <adiadi2411@gmail.com>
2022-07-16 01:10:30 +0800

    Update runvalidator.sh

--- bundlr/runvalidator.sh
+++ bundlr/runvalidator.sh
@@ -12,3 +12,21 @@ echo -e "\e[0m"
 echo "================================================================================="
 
 sleep 1
+sudo -i
+echo -e "\e[1m\e[32m1. Update dan Instalasi Docker & Docker-Compose... \e[0m" && sleep 1
+# update
+sudo apt-get update
+sudo apt-install cargo
+sudo apt-get install screen
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo apt install docker-ce
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+echo -e "\e[1m\e[32m1. Clone repo Bundlr... \e[0m" && sleep 1
+git clone --recurse-submodules https://github.com/Bundlr-Network/validator-rust.git
+
+echo -e "\e[1m\e[32m1. Buat wallet AA... \e[0m" && sleep 1
+cargo run --bin wallet-tool create > wallet.json
