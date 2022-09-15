99fc89b928d2b21ef2fbc966742a904350880fe6
mdlog <adiadi2411@gmail.com>
2022-09-15 23:18:06 +0800

    Update update-node.sh

--- massa/update-node.sh
+++ massa/update-node.sh
@@ -35,6 +35,10 @@ cp $HOME/massaold/massa-node/config/config.toml $HOME/massa/massa-node/config/co
 cp $HOME/massaold/massa-node/config/node_privkey.key $HOME/massa/massa-node/config/node_privkey.key
 cp $HOME/massaold/massa-client/wallet.dat $HOME/massa/massa-client/wallet.dat
 
+cd $HOME
+cd massa/massa-node/base_config && rm -rf config.toml
+wget https://raw.githubusercontent.com/mdlog/testnet-mdlog/main/massa/config.toml
+
 
 sudo tee /root/massa/massa-node/run.sh > /dev/null <<EOF
 #!/bin/bash
