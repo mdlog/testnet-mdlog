5cdc20aae07d3d7fedbf8585849a4fda177a0d3a
mdlog <adiadi2411@gmail.com>
2022-10-07 11:25:31 +0800

    Update update-node.sh

--- massa/update-node.sh
+++ massa/update-node.sh
@@ -23,8 +23,8 @@ rm -rf buyrolls.sh
 rm -rf /root/massa/massa-node/storage/peers.json
 
 mv massa massaold
-wget https://github.com/massalabs/massa/releases/download/TEST.14.8/massa_TEST.14.8_release_linux.tar.gz
-tar xvzf massa_TEST.14.8_release_linux.tar.gz
+wget https://github.com/massalabs/massa/releases/download/TEST.15.0/massa_TEST.15.0_release_linux.tar.gz
+tar xvzf massa_TEST.15.0_release_linux.tar.gz
 clear
 cd ~
 cd $HOME/massa/massa-node/config/ && rm -rf node_privkey.key
