68b16b6a77c1e7b799942dacbd6e0236ff781262
mdlog <adiadi2411@gmail.com>
2022-12-22 17:33:37 +0800

    Update faucet.sh

--- namada/faucet.sh
+++ namada/faucet.sh
@@ -3,14 +3,13 @@
 while true
 do
 
-
  faucet=$(namadac transfer --token NAM --amount 1000 --source faucet --target $VALIDATOR_ALIAS --signer $VALIDATOR_ALIAS)
- -Makassar123
+ echo "Makassar"
 
    printf "sleep"
         for((sec=0; sec<1; sec++))
         do
-                printf "."
+                printf "echo $faucet"
                 sleep 1
         done
         printf "\n"
