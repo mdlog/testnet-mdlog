47cabf03ec98c5c4eb02a55d2de412b5f73c812a
mdlog <adiadi2411@gmail.com>
2022-12-22 17:02:08 +0800

    Create faucet.sh

---
+++ namada/faucet.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+while true
+do
+
+
+ faucet=$(namadac transfer --token NAM --amount 1000 --source faucet --target $VALIDATOR_ALIAS --signer $VALIDATOR_ALIAS)
+ -Makassar123
+
+   printf "sleep"
+        for((sec=0; sec<1; sec++))
+        do
+                printf "."
+                sleep 1
+        done
+        printf "\n"
+done
