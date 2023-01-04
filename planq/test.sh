8d6dc40e4b1e4da5382cf9712bc57852cec61583
mdlog <adiadi2411@gmail.com>
2023-01-04 22:29:44 +0800

    Update test.sh

--- planq/test.sh
+++ planq/test.sh
@@ -7,14 +7,14 @@ read -s PASS
 while true
 do 
 WALLET=plq1k6wjxd7h49jszmue80g4d7p6mx6n0ms7a7j8e5
-BINARy=planqd
+BINARY=planqd
 KEY_NAME=mdlog
 VALOPER=plqvaloper1k6wjxd7h49jszmue80g4d7p6mx6n0ms7rqlal9
 denom_unit=aplanq
 echo $PASS | ${BINARY} tx distribution withdraw-rewards ${VALOPER} --commission --from ${WALLET} --gas auto --gas-adjustment 1.5 --chain-id=${CHAIN} --gas-prices
 
 BALANCE=$(planqd q bank balances plq1k6wjxd7h49jszmue80g4d7p6mx6n0ms7a7j8e5 | grep "amount" | awk '{ print $3 }')
-BALANCES=${BALANCE} - 300000000000000000
+BALANCES=$BALANCE - 300000000000000000
 
 if  [ $BALANCES > 0 ]; then
 echo $PASS | ${BINARY} tx staking delegate plqvaloper1k6wjxd7h49jszmue80g4d7p6mx6n0ms7rqlal9 ${BALANCES}aplanq --gas="1000000" --gas-prices="30000000000aplanq" --gas-adjustment="1.15" --chain-id planq_7070-2 --from $WALLET
