44c88b161bbf074ed292113a3ff19b1df6b4830a
mdlog <adiadi2411@gmail.com>
2023-01-04 22:33:12 +0800

    Update test.sh

--- planq/test.sh
+++ planq/test.sh
@@ -10,11 +10,12 @@ WALLET=plq1k6wjxd7h49jszmue80g4d7p6mx6n0ms7a7j8e5
 BINARY=planqd
 KEY_NAME=mdlog
 VALOPER=plqvaloper1k6wjxd7h49jszmue80g4d7p6mx6n0ms7rqlal9
-denom_unit=aplanq
-echo $PASS | ${BINARY} tx distribution withdraw-rewards ${VALOPER} --commission --from ${WALLET} --gas auto --gas-adjustment 1.5 --chain-id=${CHAIN} --gas-prices
+BAL=300000000000000000
+
+echo $PASS | ${BINARY} tx distribution withdraw-rewards ${VALOPER} --commission --from ${WALLET} --gas auto --gas-adjustment 1.5 --chain-id=${CHAIN} --gas-prices="30000000000aplanq" --chain-id planq_7070-2 --from $WALLET
 
 BALANCE=$(planqd q bank balances plq1k6wjxd7h49jszmue80g4d7p6mx6n0ms7a7j8e5 | grep "amount" | awk '{ print $3 }')
-BALANCES=$BALANCE - 300000000000000000
+BALANCES=$BALANCE - $BAL
 
 if  [ $BALANCES > 0 ]; then
 echo $PASS | ${BINARY} tx staking delegate plqvaloper1k6wjxd7h49jszmue80g4d7p6mx6n0ms7rqlal9 ${BALANCES}aplanq --gas="1000000" --gas-prices="30000000000aplanq" --gas-adjustment="1.15" --chain-id planq_7070-2 --from $WALLET
