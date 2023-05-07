8fb2f391e078a48a0e91b5c8e6e94139c1eef80b
mdlog <adiadi2411@gmail.com>
2023-05-07 19:43:37 +0800

    Update v22

--- massa/update-node.sh
+++ massa/update-node.sh
@@ -23,8 +23,8 @@ rm -rf /root/massa/massa-node/storage/peers.json
 
 mv massa massaold
 
-wget https://github.com/massalabs/massa/releases/download/TEST.21.0/massa_TEST.21.0_release_linux.tar.gz
-tar xvzf massa_TEST.21.0_release_linux.tar.gz
+wget https://github.com/massalabs/massa/releases/download/TEST.22.0/massa_TEST.22.0_release_linux.tar.gz
+tar xvzf massa_TEST.22.0_release_linux.tar.gz
 
 clear
 cd ~
