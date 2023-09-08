69ea7f879cc49af6fdd4055079a513268ad17fa8
mdlog <adiadi2411@gmail.com>
2023-09-09 00:21:32 +0800

    Update v26.1 update-node.sh

--- massa/update-node.sh
+++ massa/update-node.sh
@@ -23,8 +23,8 @@ rm -rf /root/massa/massa-node/storage/peers.json
 
 mv massa massaold
 
-wget https://github.com/massalabs/massa/releases/download/TEST.26.0/massa_TEST.26.0_release_linux.tar.gz
-tar xvzf massa_TEST.26.0_release_linux.tar.gz
+wget https://github.com/massalabs/massa/releases/download/TEST.26.1/massa_TEST.26.1_release_linux.tar.gz
+tar xvzf massa_TEST.26.1_release_linux.tar.gz
 
 clear
 cd ~
